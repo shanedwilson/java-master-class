9134e0a4b3e0e7f6a0cc3b5f9e477885fe98143d
Shane D Wilson <shane@fortyau.com>
2020-12-07 19:28:35 -0600

    section63

---
+++ Switch/src/com/shanedwilson/Main.java
@@ -0,0 +1,45 @@
+package com.shanedwilson;
+
+public class Main {
+
+    public static void main(String[] args) {
+//        int value = 3;
+//        if (value == 1) {
+//            System.out.println("Value was 1");
+//        } else if (value == 2) {
+//            System.out.println("Value was 2");
+//        } else {
+//            System.out.println("Was not 1 or 2");
+//        }
+
+        int switchValue = 3;
+
+//        switch(switchValue) {
+//            case 1:
+//                System.out.println("Value was 1");
+//                break;
+//            case 2:
+//                System.out.println("Value was 2");
+//                break;
+//            case 3: case 4: case 5:
+//                System.out.println("Was a 3, or a 4, or a 5");
+//                System.out.println("Actually, it was a " + switchValue);
+//                break;
+//            default:
+//                System.out.println("Was not 1 or 2");
+//                break;
+//        }
+
+        char challengeValue = 'C';
+
+        switch(challengeValue) {
+            case 'A': case 'B': case 'C': case 'D': case 'E':
+                System.out.println("Value was A, B, C, D or E");
+                System.out.println("Actual value was " + challengeValue);
+                break;
+            default:
+                System.out.println("Value was not found");
+                break;
+        }
+    }
+}
