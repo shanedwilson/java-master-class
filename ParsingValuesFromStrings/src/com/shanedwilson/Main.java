ada74accdaa6438c975697c27799240d193daddd
Shane D Wilson <shane@fortyau.com>
2020-12-20 11:09:09 -0600

    chapter 71

---
+++ ParsingValuesFromStrings/src/com/shanedwilson/Main.java
@@ -0,0 +1,18 @@
+package com.shanedwilson;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String numberAsString = "2018";
+        System.out.println("numberAsString= " + numberAsString);
+
+        int number = Integer.parseInt(numberAsString);
+        System.out.println(number);
+
+        numberAsString +=1;
+        number +=1;
+
+        System.out.println(numberAsString);
+        System.out.println(number);
+    }
+}
