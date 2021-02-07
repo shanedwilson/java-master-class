104997bd9cf724f9c1db0ff84ae0518af6e83aff
Shane D Wilson <shane@fortyau.com>
2021-02-06 18:34:00 -0600

    finished chapter78

---
+++ SampleCodingExercises/src/Person.java
@@ -0,0 +1,69 @@
+public class Person {
+
+    private String firstName;
+    private String lastName;
+    private int age;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        if (age < 0 || age > 100) {
+            this.age = 0;
+        } else {
+            this.age = age;
+        }
+    }
+
+    public boolean isTeen() {
+        if (this.age > 12 && this.age < 20) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public String getFullName() {
+        if (this.firstName.isEmpty() && this.lastName.isEmpty()) {
+            return "";
+        } else if (this.lastName.isEmpty()){
+            return this.firstName;
+        } else if (this. firstName.isEmpty()) {
+            return this.lastName;
+        } else {
+            return this.firstName + " " + this.lastName;
+        }
+    }
+
+    public static void main(String[] args) {
+        Person person = new Person();
+        person.setFirstName("");
+        person.setLastName("");
+        person.setAge(10);
+        System.out.println("fullName= " + person.getFullName());
+        System.out.println("teen= " + person.isTeen());
+        person.setFirstName("John");
+        person.setAge(18);
+        System.out.println("fullName= " + person.getFullName());
+        System.out.println("teen= " + person.isTeen());
+        person.setLastName("Smith");
+        System.out.println("fullName= " + person.getFullName());
+    }
+}
